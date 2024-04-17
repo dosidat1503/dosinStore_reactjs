de9821ca7ec23c274568f4fbb047f7f3fedc549f
dosidat1503 <dosidat15031712@gmail.com>
2024-04-17 16:49:57 +0700

    change baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"https://425c-14-169-15-71.ngrok-free.app/",
     headers:{
         "Content-Type":"application/json"
     }
